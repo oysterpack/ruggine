e04f067666cc8fdd47392e88951bca304274d188
Alfio Zappala <oysterpack2016@gmail.com>
2019-04-04 21:08:54 -0400

    grpc server POC - WIP - the goal is to deploy the service in k8s + istio
    services/ruggine-app-service
    - grpc service - WIP
    
    - new apps/ruggine-grpc-demo

---
+++ apps/ruggine-grpc-demo/src/main.rs
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2019 OysterPack Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#![feature(await_macro, async_await, futures_api, arbitrary_self_types)]
+
+use std::panic::catch_unwind;
+
+fn main() {
+    let _ = catch_unwind(env_logger::init);
+
+    let app = ruggine_protos_core::app!();
+    let executor = ruggine_async::global_executor();
+    let port = 50501;
+    let (server, handle) =
+        ruggine_app_service::protos::services::app::v1::grpc_server(app, executor, port);
+
+    let _ = ruggine_async::global_executor().run(server);
+}
